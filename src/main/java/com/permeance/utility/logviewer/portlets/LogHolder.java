24f7bb031ee370954d53a0be307c91f91c4fa993
cwho <cwho80@gmail.com>
2013-02-02 21:31:00 +0800

    update license and particulars

--- src/main/java/com/permeance/utility/logviewer/portlets/LogHolder.java
+++ src/main/java/com/permeance/utility/logviewer/portlets/LogHolder.java
@@ -1,20 +1,17 @@
 /**
- * Copyright (c) 2013 Permeance Technologies. All rights reserved.
+ * Copyright (C) 2013 Permeance Technologies
  * 
- * This file is part of Log Viewer.
- * 
- * Log Viewer is free software: you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either version 3 of the
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * 
- * Log Viewer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with Log Viewer. If not,
- * see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.permeance.utility.logviewer.portlets;
 
 import com.liferay.portal.kernel.log.Log;
@@ -26,6 +23,11 @@ import com.liferay.portal.kernel.util.PropsUtil;
 import java.io.CharArrayWriter;
 import java.io.Writer;
 
+/**
+ * LogHolder
+ * 
+ * @author Chun Ho <chun.ho@permeance.com.au>
+ */
 public class LogHolder {
     public static Log _log = LogFactoryUtil.getLog(LogHolder.class);
     private static LogRunnable runnable = null;
--- src/main/java/com/permeance/utility/logviewer/portlets/LogViewerControlPanelEntry.java
+++ src/main/java/com/permeance/utility/logviewer/portlets/LogViewerControlPanelEntry.java
@@ -1,18 +1,16 @@
 /**
- * Copyright (c) 2013 Permeance Technologies. All rights reserved.
+ * Copyright (C) 2013 Permeance Technologies
  * 
- * This file is part of Log Viewer.
- * 
- * Log Viewer is free software: you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either version 3 of the
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * 
- * Log Viewer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with Log Viewer. If not,
- * see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 package com.permeance.utility.logviewer.portlets;
 
@@ -21,6 +19,11 @@ import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.ControlPanelEntry;
 
+/**
+ * LogViewerControlPanelEntry
+ * 
+ * @author Chun Ho <chun.ho@permeance.com.au>
+ */
 public class LogViewerControlPanelEntry implements ControlPanelEntry {
 
     public boolean isVisible(PermissionChecker permissionChecker, Portlet portlet) throws Exception {
