b66a7832307ed6d8d753279f3c17940aee7f87a9
Tim Telcik <telcik@gmail.com>
2013-02-26 00:04:39 +0800

    Added license header.

--- src/main/java/com/permeance/utility/logviewer/portlets/PortletPropsKeys.java
+++ src/main/java/com/permeance/utility/logviewer/portlets/PortletPropsKeys.java
@@ -1,6 +1,28 @@
+/**
+ * Copyright (C) 2013 Permeance Technologies
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.permeance.utility.logviewer.portlets;
 
+
+/**
+ * PortletPropsKeys
+ * 
+ * @author Chun Ho <chun.ho@permeance.com.au>
+ */
 public class PortletPropsKeys {
+	
     public static final String PERMEANCE_LOG_VIEWER_REFRESH_INTERVAL = "permeance.log.viewer.refreshIntervalMs";
 
     public static final String PERMEANCE_LOG_VIEWER_AUTOATTACH_ENABLED = "permeance.log.viewer.autoattach";
--- src/main/java/com/permeance/utility/logviewer/portlets/PortletPropsValues.java
+++ src/main/java/com/permeance/utility/logviewer/portlets/PortletPropsValues.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2013 Permeance Technologies
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.permeance.utility.logviewer.portlets;
 
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -5,6 +20,12 @@ import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.text.DecimalFormat;
 
+
+/**
+ * PortletPropsValues
+ * 
+ * @author Chun Ho <chun.ho@permeance.com.au>
+ */
 public class PortletPropsValues {
 
     public static final String PERMEANCE_LOG_VIEWER_PATTERN = GetterUtil.getString(
@@ -21,4 +42,5 @@ public class PortletPropsValues {
 
     public static final String PERMEANCE_LOG_VIEWER_REFRESH_INTERVAL_DISPLAY_SECONDS = (new DecimalFormat("##0.0#"))
             .format(PERMEANCE_LOG_VIEWER_REFRESH_INTERVAL / 1000d);
+    
 }
